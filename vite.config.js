7f7f4489948fe39e173a42bc57e6dd1bfca59ead
gutlibe <gutlibe24@gmail.com>
2025-04-07 21:25:38 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,36 @@
 import { defineConfig } from 'vite';
+import { obfuscator } from 'rollup-obfuscator';
 
 export default defineConfig({
+  plugins: [
+    obfuscator({
+      optionsPreset: 'medium-obfuscation',
+      sourceMap: false,
+      exclude: ['node_modules/**'] // Default, can be omitted
+    })
+  ],
   root: '.',
   publicDir: 'public',
-  
   build: {
     outDir: 'dist',
-    sourcemap: false, // Set to true if you want source maps for debugging production builds
-    cssCodeSplit: false, // Keep CSS as one file if desired
-    assetsInlineLimit: 0, // Ensure assets are not inlined as base64
-    
+    sourcemap: false,
+    cssCodeSplit: false,
+    assetsInlineLimit: 0,
     rollupOptions: {
       input: {
         main: 'index.html'
-        // If you still have the separate login page, add it here
-        // login: 'login/index.html'
       },
       output: {
         entryFileNames: `assets/js/[name].[hash].js`,
         chunkFileNames: `assets/js/[name].[hash].js`,
         assetFileNames: `assets/[ext]/[name].[hash].[ext]`,
       },
-      plugins: [
-        // Obfuscator plugin removed
-      ]
+      plugins: [] // No plugins needed here, as obfuscator is in top-level
     }
   },
-  
   css: {
     preprocessorOptions: {}
-    // If you want Vite to handle CSS minification instead of lightningcss:
-    // postcss: {
-    //   plugins: [
-    //     require('cssnano')({ // You might need to npm install --save-dev cssnano
-    //       preset: 'default',
-    //     }),
-    //   ],
-    // },
   },
-  
   server: {
     open: true
   }
